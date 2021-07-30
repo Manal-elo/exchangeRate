bcfcab0f949fc116ba253b3354805211dedec56a
manal elouardani <manalelouardani@uir.ac.ma>
2021-07-30 04:04:28 +0100

    ll

--- exchange_rate/ExchangeRateTableViewCell.swift
+++ exchange_rate/ExchangeRateTableViewCell.swift
@@ -9,7 +9,79 @@ import UIKit
 
 
 
-class ExchangeRateTableViewController: UITableViewController {
+class ExchangeRateTableViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var pickerViewButton: UIButton!
+    let screenWidth = UIScreen.main.bounds.width - 10
+    let screenHeight = UIScreen.main.bounds.height / 2
+    var selectedRow = 0
+    
+
+    
+    var currencies = [
+        "USD",
+        "EUR"
+    ]
+    @IBAction func popUpPi(_ sender: Any) {
+    
+        let vc = UIViewController()
+        vc.preferredContentSize = CGSize(width: screenWidth, height: screenHeight)
+        let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: screenWidth, height:screenHeight))
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        
+        pickerView.selectRow(selectedRow, inComponent: 0, animated: false)
+        //pickerView.selectRow(selectedRowTextColor, inComponent: 1, animated: false)
+        
+        vc.view.addSubview(pickerView)
+        pickerView.centerXAnchor.constraint(equalTo: vc.view.centerXAnchor).isActive = true
+        pickerView.centerYAnchor.constraint(equalTo: vc.view.centerYAnchor).isActive = true
+        
+        let alert = UIAlertController(title: "Select Background Colour", message: "", preferredStyle: .actionSheet)
+        
+        alert.popoverPresentationController?.sourceView = pickerViewButton
+        alert.popoverPresentationController?.sourceRect = pickerViewButton.bounds
+        
+        alert.setValue(vc, forKey: "contentViewController")
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (UIAlertAction) in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Select", style: .default, handler: { (UIAlertAction) in
+            self.selectedRow = pickerView.selectedRow(inComponent: 0)
+            //self.selectedRowTextColor = pickerView.selectedRow(inComponent: 1)
+            _ = Array(self.currencies)[self.selectedRow]
+            //let selectedTextColor = Array(self.backGroundColours)[self.selectedRowTextColor]
+  
+      
+      
+        
+            //self.pickerViewButton.setTitleColor(selectedTextColor.value, for: .normal)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView
+    {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 30))
+        label.text = Array(currencies)[row]
+        label.sizeToFit()
+        return label
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1 //return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        currencies.count
+    }
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat
+    {
+        return 60
+    }
+    
     
     var exchangeRates = [String]()
     
@@ -76,6 +148,7 @@ class ExchangeRateTableViewController: UITableViewController {
         
        
         cell.textLabel?.text = exchangeRates[indexPath.row]
+        
  
         return cell
     }
